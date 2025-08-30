c920830a3cdcb9ee70d9359cab80d61e73203a72
skepee <skepee01@gmail.com>
2025-08-30 19:17:08 +0100

    initial commit

---
+++ src/auth.js
@@ -0,0 +1,7 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { app } from "./firebase";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, signInWithPopup, signOut };
