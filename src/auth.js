fec98051a66eabab196776e5b50cb4dc6ed92f90
skepee <skepee01@gmail.com>
2025-08-31 21:50:20 +0100

    tidy up

--- src/auth.js
+++
@@ -1,7 +0,0 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { app } from "./firebase";
-
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider, signInWithPopup, signOut };
