0bf5c134b75f4b0ed06f4e7b23f663803c402b79
skepee <skepee01@gmail.com>
2025-09-02 17:39:18 +0100

    ads

--- web/src/App.js
+++ web/src/App.js
@@ -3,24 +3,7 @@ import { auth, provider, signInWithPopup, signOut } from './auth';
 import { db } from './firebase';
 import { collection, addDoc, Timestamp, query, orderBy, limit, getDocs } from 'firebase/firestore';
 import './App.css';
-
-// function AdBanner() {
-//   const adRef = useRef(null);
-//   useEffect(() => {
-//     if (window.adsbygoogle && adRef.current) {
-//       window.adsbygoogle.push({});
-//     }
-//   }, []);
-//   return (
-//     <ins className="adsbygoogle"
-//       style={{ display: 'block' }}
-//       data-ad-client="ca-pub-1917839501702299"
-//       data-ad-slot="8369048135"
-//       data-ad-format="auto"
-//       data-full-width-responsive="true"
-//       ref={adRef}></ins>
-//   );
-// }
+import AdBanner from './AdBanner';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -227,26 +210,6 @@ function App() {
       ))}
     </div>
   );
-
-  function AdBanner() {
-    const adRef = useRef(null);
-
-    useEffect(() => {
-      if (window.adsbygoogle && adRef.current) {
-        window.adsbygoogle.push({});
-      }
-    }, []);
-
-    return (
-      <ins className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-1917839501702299"
-        data-ad-slot="8369048135"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-        ref={adRef}></ins>
-    );
-  }
 
   return (
     <div className="App">
